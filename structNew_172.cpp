26f4ee42b2d86fce8da25aa5517a54b83015673d
deonugroho <deonugroz888@gmail.com>
2026-04-06 16:33:06 +0700

    Membuat file struct

---
+++ structNew_172.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct Orang
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main() {
+    Orang mhs;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat : ";
+    cin >> mhs.alamat;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Alamat : " << mhs.alamat << endl;
+    cout << " Umur : " << mhs.umur << endl;
+}
